65ce0bd5b4424aff1b34f75cc36aca47e30ddb1b
Brandon <verv0022@algonquinlive.com>
2021-05-10 13:58:13 -0400

    page layout

--- src/components/Sections/Contact/Contact.jsx
+++ src/components/Sections/Contact/Contact.jsx
@@ -5,52 +5,60 @@ import ContactMe from "../../../images/Icons/contactme.svg"
 const Contact = () => (
   <div className={styles.container} id="contactSection">
     <div className={styles.content}>
-      <h1 className={styles.title}>Let's get in touch! </h1>
       <div className={styles.contactContainer}>
-        <ContactMe alt="Contact Me Image" className={styles.contactMeImage} />
-        <form
-          name="contact-form"
-          method="POST"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="bot-field" />
-          <input type="hidden" name="form-name" value="contact-form" />
-          <div className={styles.field}>
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="John Smith"
-              className={styles.textInput}
-            />
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              placeholder="youremail@domain"
-              className={styles.textInput}
-            />
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="message">Message:</label>
-            <textarea
-              name="message"
-              id="message"
-              rows="6"
-              placeholder="Your Message"
-            />
-          </div>
-          <div className={styles.actions}>
-            <button type="submit" className="special" id={styles.submit}>
-              Send Message
-            </button>
-          </div>
-        </form>
+        <div className={styles.imageContainer}>
+          <h1 className={styles.title}>Let's get in touch! </h1>
+          <h2 className={styles.subTitle}>
+            Shoot me a message if you would like to work with me, have any
+            questions, or simply want to connect.
+          </h2>
+          <ContactMe alt="Contact Me Image" className={styles.contactMeImage} />
+        </div>
+        <div className={styles.formContainer}>
+          <form
+            name="contact-form"
+            method="POST"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact-form" />
+            <div className={styles.field}>
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="John Smith"
+                className={styles.textInput}
+              />
+            </div>
+            <div className={styles.field}>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="youremail@domain"
+                className={styles.textInput}
+              />
+            </div>
+            <div className={styles.field}>
+              <label htmlFor="message">Message:</label>
+              <textarea
+                name="message"
+                id="message"
+                rows="6"
+                placeholder="Your Message"
+              />
+            </div>
+            <div className={styles.actions}>
+              <button type="submit" className="special" id={styles.submit}>
+                Send Message
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
