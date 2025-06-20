9e3e5b22eb0ccf42a3fc94db569617f162767583
Brandon <verv0022@algonquinlive.com>
2025-06-20 18:30:08 -0400

    -updated waves/bubble graphics, -updated about me -added node version

--- src/components/Sections/About/About.jsx
+++ src/components/Sections/About/About.jsx
@@ -46,29 +46,14 @@ const About = () => {
           <div className={styles.aboutMeText}>
             <h1 className={styles.title}>About Me</h1>
             <p>
-              Hello! My name is Brandon Vervoort. I’m a perfectionist who loves
-              turning ideas into reality. To do this my main tool is JavaScript
-              and its frame-works such as React but JavaScript isn't the only
-              tool I use, I know my way around Photoshop, Illustrator, and
-              prototyping tools such as XD and Figma. I have a strong balance of
-              creative and technical knowledge, I’m a coder who loves design.
-            </p>
-            <br />
-            <p>
-              I graduated from the{" "}
-              <a
-                href="https://www.algonquincollege.com/mediaanddesign/program/mobile-application-design-and-development/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.collegeLink}
-              >
-                Mobile Application Design & Development
-              </a>{" "}
-              program at Algonquin College in Ottawa. During my studies I
-              learned full-stack development of web and mobile applications as
-              well as design principles. I then worked on a team to develop a
-              voice assistant for the Canadian Border Service Agency that helps
-              users get border rules and regulations hands-free.
+              I'm a developer with a strong focus on mobile and front-end
+              technologies, specializing in native iOS and cross-platform
+              applications. With over three years of experience, I’ve built
+              accessible, user-focused solutions for government and freelance
+              projects, turning thoughtful designs into clean, functional code.
+              I place a strong emphasis on user experience and thrive at the
+              intersection of design and development, always looking for
+              challenges that blend creativity, usability, and problem solving.
             </p>
           </div>
           <AboutMeImage className={styles.aboutMeImage} />
