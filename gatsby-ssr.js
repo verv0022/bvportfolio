1340c4ec5116b5c9e49a52d13c4e0a6c973cd0b1
Brandon <verv0022@algonquinlive.com>
2025-06-21 19:58:14 -0400

    Fix: ensure consistent SSR/browser wrapRootElement for layout stability

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import { ParallaxProvider } from "react-scroll-parallax"
+
+import "./src/layout.css"
+
+export const wrapRootElement = ({ element }) => {
+  return <ParallaxProvider>{element}</ParallaxProvider>
+}
